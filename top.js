b1d5103ac6952e6026826529e4cb57d7602f6d93
MIYA2021 <80123162+MIYA2021@users.noreply.github.com>
2024-07-08 14:33:41 +0900

    Update top.js

--- top.js
+++ top.js
@@ -53,20 +53,4 @@ $(function(){
       }
     ]
   });
-
-  /*=================================================
-    スクロール時の画像フェード表示
-    ===================================================*/
-  $(window).scroll(function() {
-    $('.fadein').each(function() {
-      let scroll = $(window).scrollTop();
-      let target = $(this).offset().top;
-      let windowHeight = $(window).height();
-      if (scroll > target - windowHeight + 200) {
-        $(this).css('opacity','1');
-        $(this).css('transform','translateY(0)');
-      }
-    });
-  });
-});
 })
